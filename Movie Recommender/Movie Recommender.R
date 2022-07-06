656f8ddd84c0905211668646d5009802ac1fa525
wbgephart <99994662+wbgephart@users.noreply.github.com>
2022-07-06 14:49:17 -0400

    Update Movie Recommender.R

--- Movie Recommender/Movie Recommender.R
+++ Movie Recommender/Movie Recommender.R
@@ -1,9 +1,3 @@
-# Building a Recommender System in R
-
-# In this project, I am going to build a recommender system that will recommend movies to users. This will show my strategy for preprocessing data, exploring data, and creating thie system itself using 'recommenderLab'
-
-#'recommenderLab' is a R package that provides a framework to test and develop recommender algorithms. Various algorithms are supported, invluding User-based collaborative filtering (UBCF), item-based collaborative filtering (IBCF), Association rule-based recommender (AR), and many more. It was developed in 2016 by Micharl Hahsler.
- 
 ## Load Packages
 install.packages("aws.s3")
 install.packages("tidyverse")
@@ -77,8 +71,6 @@ movies_full <- movies %>%
 
 
 ### Recommender engine and predictions
-## When doing a market based analysis you want to look at the items in your current basket and predict the next best item(s) that would fit with the present items in the basket. This is a simplified version of item-based collaborative filtering; collecting preferences or taste information from many users allows you to make associations between the present items and associated items. Instead of items in a basket, we hve genres in a movie, and the model will output which genre is most often associated with the input genres. 
-
 
 ### 1. Set up recommender engine and perform item based collaborative filtering
 recom <- Recommender(movies_matrix, method = "IBCF", param = list(k = 5))
